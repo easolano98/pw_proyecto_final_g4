917fa91e1199a9225ba18093bf6e86e293f50a9a
easolano98 <solano_erick@hotmail.com>
2023-09-04 19:05:30 -0500

    Estilos NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,48 @@
 <template>
-  <router-link to="/estudiantes/registro">Suscripcion</router-link>
-  <router-link to="/foros">Foros</router-link>
-  <router-link to="/noticias">Noticias</router-link>
-  <router-link to="/quejas">Quejas</router-link>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <router-link class="nav-link active" aria-current="page" to="/"><img src="../assets/UCE.png" alt="no se puede mostrar la imagen"></router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="/estudiantes/registro">Suscripcion</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="/foros">Foros</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="/noticias">Noticias</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page"  to="/quejas">Quejas</router-link>
+          </li>
+        </ul>
+       
+      </div>
+      
+    </div>
+  </nav>
 </template>
 
 <script>
-export default {};
+export default {
+
+}
 </script>
 
 <style>
+ nav{
+    margin-top: -60px;
+    font-weight: bold;
+  }
+
+  img{
+    width: 75px;
+    height: 75px;
+  }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import router from "./routers/router"
 createApp(App).use(router).mount('#app')
